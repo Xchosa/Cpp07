c3b2998c14e0f376d554937b5458de3297a56aa5
Paul <paul.overbeck95@gmail.com>
2025-11-30 20:49:27 +0100

    ex00 done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,15 +6,49 @@
 /*   By: poverbec <poverbec@student.42heilbronn>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 16:26:46 by poverbec          #+#    #+#             */
-/*   Updated: 2025/11/27 16:28:12 by poverbec         ###   ########.fr       */
+/*   Updated: 2025/11/30 20:44:12 by poverbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
 
+//int main()
+//{
+//	double a = 2;
+//	double b = 3;
+//	std::cout << "a = " << a << ", b = " << b << std::endl;
+//	std::cout << "min(a, b) = " <<  min<double>(a, b) << std::endl;
+//	std::cout << "max(a, b) = " << max<double>(a, b) << std::endl;
+	
+//	std::cout << "swap values" << std::endl;
+//	swap(a, b);
+//	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+//	int c = -10;
+//	int d = 3;
+//	std::cout << "c = " << c << ", d = " << d << std::endl;
+//	std::cout << "min(c, d) = " << min<int>(c, d) << std::endl;
+//	std::cout << "max(c, d) = " << max<int>(c, d) << std::endl;
+
+//	return 0;
+//}
+
 int main()
 {
-	
-	return 0;
+    int a = 2;
+    int b = 3;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+    std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+
+    return 0;
 }
--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,10 +6,28 @@
 /*   By: poverbec <poverbec@student.42heilbronn>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 16:27:03 by poverbec          #+#    #+#             */
-/*   Updated: 2025/11/27 16:27:44 by poverbec         ###   ########.fr       */
+/*   Updated: 2025/11/30 20:38:09 by poverbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once 
 
 #include <iostream>
+
+template <typename T> T max(T x, T y)
+{
+	return (x > y) ? x : y;
+}
+
+
+template <typename T> T min(T x, T y)
+{
+	return (x < y) ? x : y;
+}
+
+template <typename T> void swap(T &x, T &y)
+{
+	T tmp = x;
+	x = y;
+	y = tmp;
+}
