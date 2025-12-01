24e27c413abc7f9cc71fbf814fc3e3181ceaee1f
Paul Overbeck <poverbec@2-B-6.42heilbronn.de>
2025-12-01 15:22:49 +0100

    ex01 done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,19 +6,25 @@
 /*   By: poverbec <poverbec@student.42heilbronn>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 16:26:46 by poverbec          #+#    #+#             */
-/*   Updated: 2025/12/01 11:37:11 by poverbec         ###   ########.fr       */
+/*   Updated: 2025/12/01 15:19:14 by poverbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include <string>
 
-template <typename T> void testfunction(T &array_value)
+template <typename T > 
+void testfunction(T &array_value)
 {
     array_value = array_value *2;
 }
+//void constfunction(T &array_value)
+//{
+//    std 
+//}
 
-template <typename T> void printfk(T &array)
+template <typename T> 
+void printfk(T array)
 {
     std::cout<< array << ", ";
 }
@@ -27,20 +33,14 @@ int main()
 {
     double testarray[] = {1,2,3,4,5,6,7,8,9,10};
     std::string stringarray[] = {"hello", "world", "shine"};
+    int const constArray[] = {1,2,3,4,5,6,7,8,9,10};
 
     std::cout << "print array before iter" << std::endl;
-    for(int i = 0; i < 10; i ++)
-    {
-       std::cout << testarray[i ]<< ", ";
-    }
+    iter(testarray, 10, printfk<double>);
     std::cout << std::endl;
     iter(testarray, 10, testfunction<double>);
     
     std::cout << "print array after iter" << std::endl;
-    for(int i = 0; i < 10; i ++)
-    {
-        std::cout << testarray[i ]<< ", ";
-    }
     iter(testarray, 10, printfk<double>);
     std::cout << std::endl;
 
@@ -49,7 +49,12 @@ int main()
     
     std::cout << StringArrayLength<< std::endl;
     iter(stringarray, StringArrayLength, printfk<std::string>);
-    
+
+    std::cout << std::endl;
+    std::cout << "print Const Int array" << std::endl;
+    iter(constArray, 10, printfk<int>);
+    std::cout << std::endl;
+
 	return 0;
 }
 
