2ce71b6d155904f14eccf1576b64e66e86548689
Paul Overbeck <poverbec@2-B-6.42heilbronn.de>
2025-12-01 16:40:42 +0100

    ex02

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,35 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: poverbec <poverbec@student.42heilbronn>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 16:27:03 by poverbec          #+#    #+#             */
-/*   Updated: 2025/12/01 14:25:30 by poverbec         ###   ########.fr       */
+/*   Updated: 2025/12/01 16:20:24 by poverbec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once 
 
 #include <iostream>
+#include <exception>
 
-template <typename T> T max(T x, T y)
-{
-	return (x > y) ? x : y;
-}
+template <typename T>
+class Array{
+	
+	private:
+	unsigned int _n;
 
+	public:
+	Array(){};
+	~Array(){};
 
-template <typename T> T min(T x, T y)
-{
-	return (x < y) ? x : y;
-}
-
-template <typename T> void swap(T &x, T &y)
-{
-	T tmp = x;
-	x = y;
-	y = tmp;
+	Array& Array(const Array &object);
+	Array& operator=(const Array &object)
+	{
+		
+	}
 }
 //template <typename T> void iter(T *array, int length , void (*function) (T&))
 //{
@@ -40,14 +40,23 @@ template <typename T> void swap(T &x, T &y)
 //	}
 //}
 
-// generic function Pointer 
-// deduce 
-template <typename T, typename FuncType>
-void iter( T *array, int length, FuncType function)
-{
+//const char* ExceptionOutofRange::what() const throw()
+//{
+//	return ErrorMessage.c_str();
+//}
+
+//class ExceptionOutofRange(int index) : public std::exception
+//{
+//	ErrorMessage = "out of bounds indes: " + std::to_string(index);
+//}
+//// generic function Pointer 
+//// deduce 
+//template <typename T, typename FuncType>
+//void iter( T *array, int length, FuncType function)
+//{
 	
-	for(int i = 0; i < length; i++)
-	{
-		function(array[i]);
-	}
-}
+//	for(int i = 0; i < length; i++)
+//	{
+//		function(array[i]);
+//	}
+//}
